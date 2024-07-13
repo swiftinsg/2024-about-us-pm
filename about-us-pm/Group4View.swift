b0886e6750f8c198a7303425d152eaa2f5df6728
yay <61081228+yanganyi@users.noreply.github.com>
2024-07-13 16:16:42 +0800

    update my own swift file

--- about-us-pm/Group4View.swift
+++ about-us-pm/Group4View.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct Group4View: View {
     var body: some View {
         TabView {
-            /*@START_MENU_TOKEN@*/Text("This is person 1! Replace me with a view.")/*@END_MENU_TOKEN@*/
+            YAYView()
                 .tabItem {
-                    Label("Person 1", systemImage: "person.circle.fill")
+                    Label("YAY", systemImage: "person.circle.fill")
                 }
             
             /*@START_MENU_TOKEN@*/Text("This is person 2! Replace me with a view.")/*@END_MENU_TOKEN@*/
---
+++ about-us-pm/Group4YAY.swift
@@ -0,0 +1,20 @@
+//
+//  Group4YAY.swift
+//  about-us-pm
+//
+//  Created by Yang An Yi on 13/7/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct YAYView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                Text("Hi I'm An Yi, J1 from Hwa Chong Institution, Challenge 1 and 2 both Group 4!")
+            }.navigationTitle("Not YAY")
+        }
+    }
+}
