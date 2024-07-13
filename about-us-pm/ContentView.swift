00e1f61c98970657badb9a6fd47e5df548c5903c
Jia Chen <github@jiachen.app>
2024-07-13 11:02:08 +0800

    Initial Commit

---
+++ about-us-pm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  about-us-pm
+//
+//  Created by Jia Chen Yee on 13/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ about-us-pm/about_us_pmApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_us_pmApp.swift
+//  about-us-pm
+//
+//  Created by Jia Chen Yee on 13/7/24.
+//
+
+import SwiftUI
+
+@main
+struct about_us_pmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
