65b56af891a5e8278b699fa4c0298d4675ad71f1
Sophie <tofuisshartingbecauseofthemala@gmail.com>
2024-07-13 16:48:26 +0800

    added my intro

--- about-us-pm/SophieView.swift
+++ about-us-pm/SophieView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct SophieView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello I'm Sophie")
+            .bold()
+            .padding()
+            
     }
 }
 
