2033758fe1ed054ac2ff151ca22e69107d3644e8
VJ2408 <vijai.adithya24@gmail.com>
2024-07-13 16:16:47 +0800

    Group 1, 1pm

--- about-us-pm/Group1QiYang.swift
+++ about-us-pm/Group1QiYang.swift
@@ -5,4 +5,18 @@
 //  Created by Vijai Adithya on 13/7/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct Group1QiYang: View {
+    
+    @State private var scale = 1.0
+    
+    var body: some View {
+        Text("Hello, I am Vijai")
+        
+    }
+}
+
+#Preview {
+    InstructorMentorView()
+}
